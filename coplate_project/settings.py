5038f043f67d234ee0c002094a3dcdce215529ee
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-06 09:32:38 -0500

    set up login page and redirect to websitepage

--- coplate_project/settings.py
+++ coplate_project/settings.py
@@ -136,6 +136,9 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+ACCOUNT_SIGNUP_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "index"
+
 #Email settings
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
