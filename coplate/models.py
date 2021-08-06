2cfda76a2b28a65622656acfe8012ee86e7bd928
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-06 10:31:11 -0500

    add nickname field

--- coplate/models.py
+++ coplate/models.py
@@ -5,4 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=15, unique=True, null=True)
+
+    def __str__(self):
+        return self.email
