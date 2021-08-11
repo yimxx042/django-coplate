bcfc1baff4c818729b078df22441822a3da7bf23
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-11 11:54:24 -0500

    add messages/overwrite

--- coplate_project/settings.py
+++ coplate_project/settings.py
@@ -142,6 +142,9 @@ ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = "account_email_confirmat
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account_email_confirmation_done"
 PASSWORD_RESET_TIMEOUT_DAYS = 3
 
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account_email_confirmation_done"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
+
 #Email settings
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
