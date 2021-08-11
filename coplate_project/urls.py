b5b92498a71bdb9812cd4f82af4094e39a725d8c
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-11 09:57:21 -0500

    add email verification, email time setting, password redirection

--- coplate_project/urls.py
+++ coplate_project/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from coplate.views import CustomPasswordChangeView
 
 urlpatterns = [
+    #admin
     path('admin/', admin.site.urls),
+    #coplate
     path('', include('coplate.urls')),
+    #allauth
+    path('email-confirmation-done/', 
+    TemplateView.as_view(template_name="coplate/email_confirmation_done.html"),
+    name='account_email_confirmation_done',
+    ),
+    path('password/change/', CustomPasswordChangeView.as_view(), 
+    name="account_password_change"
+    ),
     path('', include('allauth.urls')),
 ]
